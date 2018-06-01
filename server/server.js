cedad94f59259b61c42b2d2ee49c8fdc3db2c4ac
Roshan Paturkar <paturkarr@gmail.com>
2018-06-01 22:08:46 +0530

    Setting app for heroku

--- server/server.js
+++ server/server.js
@@ -2,15 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(publicPath));
 
-// app.get('/', (request, response) => {
-//   response.send('Welcome to chat app!')
-// });
-
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
